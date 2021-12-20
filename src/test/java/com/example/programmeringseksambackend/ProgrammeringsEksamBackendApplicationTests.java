4bfd1e6b059cc2d1a30c4de5430790099216fb9e
Snimb <72379945+Snimb@users.noreply.github.com>
2021-12-20 08:54:29 +0100

    Initial commit

---
+++ src/test/java/com/example/programmeringseksambackend/ProgrammeringsEksamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.programmeringseksambackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammeringsEksamBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
